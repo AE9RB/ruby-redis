4743762b2401703b986f2a0d5985f0014aa6f02f
David Turnbull <dturnbull@gmail.com>
2011-07-16 18:31:13 -0700

    faster and nicer

--- lib/redis/send.rb
+++ lib/redis/send.rb
@@ -7,43 +7,41 @@ class Redis
     def send_redis data
       collect = []
       if nil == data
-        collect << "$-1\r\n"
+        collect << "$-1"
       elsif false == data
-        collect << ":0\r\n"
+        collect << ":0"
       elsif true == data
-        collect << ":1\r\n"
+        collect << ":1"
       elsif Float === data and data.nan?
-        collect << ":0\r\n"
+        collect << ":0"
       elsif Float === data and data.infinite? || 0 > 0
-        collect << ":inf\r\n"
+        collect << ":inf"
       elsif Float === data and data.infinite? || 0 < 0
-        collect << ":-inf\r\n"
+        collect << ":-inf"
       elsif Hash === data
-        collect << "*#{data.size * 2}\r\n"
+        collect << "*#{data.size * 2}"
         data.each do |key, value|
           key = key.to_s
           value = value.to_s
-          collect << "$#{key.bytesize}\r\n"
+          collect << "$#{key.bytesize}"
           collect << key
-          collect << "\r\n$#{value.bytesize}\r\n"
+          collect << "$#{value.bytesize}"
           collect << value
-          collect << "\r\n"
         end
       elsif Enumerable === data and !(String === data)
-        collect << "*#{data.size}\r\n"
+        collect << "*#{data.size}"
         data.each do |element|
           element = element.to_s
-          collect << "$#{element.bytesize}\r\n"
+          collect << "$#{element.bytesize}"
           collect << element
-          collect << "\r\n"
         end
       else
         data = data.to_s
-        collect << "$#{data.bytesize}\r\n"
+        collect << "$#{data.bytesize}"
         collect << data
-        collect << "\r\n"
       end
-      send_data collect.join
+      collect << ''
+      send_data collect.join "\r\n"
     end
     
   end
